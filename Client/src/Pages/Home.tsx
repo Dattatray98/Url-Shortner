32f89cb2230d9df1317e929adedac066547af798
jojewar Dattatray <jojewardattatray@gmail.com>
2025-08-27 16:29:33 +0530

    updated

--- Client/src/Pages/Home.tsx
+++ Client/src/Pages/Home.tsx
@@ -157,7 +157,6 @@ const Home = () => {
             <div className="mt-5 max-h-[38vh] space-y-4 overflow-y-scroll p-5 scrollbar-hidden">
               {recentUrls.map((link: Link) => (
                 <div
-                  key={link._id}
                   className="border border-gray-300 mx-2 py-3 px-5 rounded-xl flex justify-between items-center"
                 >
                   {/* Original */}
